b0dd9ed1ddeda5dfd7971533bc5e7c697a320376
muhammadmujir <muhammadmujir011@gmail.com>
2022-03-25 22:42:18 +0700

    create module constant

--- routes/user_bp.py
+++ routes/user_bp.py
@@ -7,13 +7,14 @@ Created on Wed Mar  9 20:59:57 2022
 
 from flask import Blueprint
 from controllers.UserController import *
+from utils.constant import BASE_URL
 
-user_bp = Blueprint('user_bp', __name__, url_prefix="/users")
+user_bp = Blueprint('user_bp', __name__, url_prefix=BASE_URL+"/users")
 user_bp.route('/', methods=['GET'])(index)
 user_bp.route('/create', methods=['GET'])(store)
 user_bp.route('/_currentUser', methods=['GET'])(show)
 user_bp.route('/<int:user_id>/edit', methods=['POST'])(update)
 user_bp.route('/<int:user_id>', methods=['DELETE'])(delete)
 # auth
-user_bp.route('/register', methods=['POST'])(register)
-user_bp.route('/login', methods=['POST'])(login)
+user_bp.route('/_register', methods=['POST'])(register)
+user_bp.route('/_login', methods=['POST'])(login)
---
+++ utils/constant.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 25 22:19:24 2022
+
+@author: Admin
+"""
+
+BASE_URL = "/api/v1.0"
