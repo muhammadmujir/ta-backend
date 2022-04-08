4050f5493070ba022f69ae6d456d9b555567e381
muhammadmujir <muhammadmujir011@gmail.com>
2022-04-08 13:56:18 +0700

    trial websocket final

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Created on Wed Oct  6 20:35:23 2021
 """
 
 #Import necessary libraries
-from flask import Flask, render_template, Response, request, jsonify
+from flask import Flask, render_template, Response, request, jsonify, session, copy_current_request_context
 from multiprocessing import Process
 import cv2
 import torch
@@ -24,6 +24,8 @@ import numpy as np
 import PIL.Image as Image
 from matplotlib import cm
 import time
+from flask_socketio import SocketIO, emit, disconnect, join_room, leave_room
+from threading import Lock
 
 #Initialize the Flask app
 app = Flask(__name__)
@@ -33,14 +35,18 @@ app.register_blueprint(exception_bp)
 db = Database().db
 db.init_app(app)
 migrate = Migrate(app, db)
+async_mode = None
+socketio = SocketIO(app, async_mode=async_mode)
+thread = dict()
+thread_lock = Lock()
 
-camera = cv2.VideoCapture("G:\\videoplayback.mp4")
+camera = cv2.VideoCapture(0)
 # if not camera.isOpened():
 #     print("Cannot open camera")
     
 #init model
 model = InceptionResNetV2().cpu()
-checkpoint = torch.load("G:\\Dataset\\Result\\0model_best.pth.tar")
+checkpoint = torch.load("D:\\TA\\Dataset\\Result\\0model_best.pth.tar")
 model.load_state_dict(checkpoint['state_dict'])
 transform=transforms.Compose([
                       transforms.ToTensor(),transforms.Normalize(
@@ -50,7 +56,7 @@ transform=transforms.Compose([
 
 
 def gen_frame():
-    delay = 60 # 1 minute
+    delay = 10 # 1 minute
     start = time.time()
     while True:
         success, frame = camera.read()
@@ -73,7 +79,11 @@ def gen_frame():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('websocket.html', async_mode=async_mode)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
@@ -101,5 +111,78 @@ def handle_404_error(err):
     response = {"code": 404, "status": "Not Found", "data": None, "errors": ["Not Found"]}
     return jsonify(response), 404
 
+def calculate_crowd_counting(message):
+    start = time.time()
+    while True:
+        socketio.sleep(3)
+        # with app.app_context():
+        #     socketio.emit('my_response', {'data': message['data'], 'count': session.get('receive_count', 0) }, namespace='/test')
+        with app.test_request_context('/'):
+            session['receive_count'] = session.get('receive_count', 0) + 1
+            socketio.emit('my_response', {'data': message['data'], 'count': session.get('receive_count', 0) }, namespace='/test', room=message['data'])
+        # if (time.time() - start == 30):
+        #     session['receive_count'] = session.get('receive_count', 0) + 1
+        #     emit('my_response',
+        #           {'data': 'Streaming', 'count': session['receive_count']})
+        #     start = time.time()
+        
+        # if (session.get('receive_count', 0) > 10):
+        #     break
+        
+@socketio.on('my_event', namespace='/test')
+def test_message(message):
+    global thread
+    join_room(message['data'])
+    # with thread_lock:
+    #     # print("masuk event")
+    #     if thread is None:
+    #         thread = socketio.start_background_task(calculate_crowd_counting(message))
+    # thread_lock.acquire()
+    if not message['data'] in thread.keys():
+        thread[message['data']] = socketio.start_background_task(calculate_crowd_counting(message))
+    # thread_lock.release()
+    # session['receive_count'] = session.get('receive_count', 0) + 1
+    # emit('my_response',
+    #       {'data': message['data'], 'count': session['receive_count']})
+
+@socketio.on('join', namespace='/test')
+def join():
+    print("berhasil join")
+    join_room("mujir", namespace='/test')
+
+@socketio.on('leave', namespace='/test')
+def leave():
+    print("berhasil leave")
+    leave_room("mujir", namespace='/test')
+    
+@socketio.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socketio.on('connect', namespace='/test')
+def connect():
+    # global thread
+    # with thread_lock:
+    #     if thread is None:
+    #         thread = socketio.start_background_task(calculate_crowd_counting)
+    emit('my_response',
+          {'data': 'Connected', 'count': session.get('receive_count', 0)})
+    
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']},
+         callback=can_disconnect)
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    socketio.run(app, ssl_context=None, debug=True)
