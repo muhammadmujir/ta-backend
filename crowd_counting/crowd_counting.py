aa11e1fb62059a36876b55bb8a39c3904050caf2
muhammadmujir <muhammadmujir011@gmail.com>
2022-06-26 17:57:18 +0700

    bugfix

--- crowd_counting/crowd_counting.py
+++ crowd_counting/crowd_counting.py
@@ -5,11 +5,14 @@ Created on Tue Apr 19 01:08:45 2022
 @author: Admin
 """
 from torchvision import datasets, transforms
-from crowd_counting.inceptionresnetv2 import InceptionResNetV2
+from inceptionresnetv2 import InceptionResNetV2
+from vgg16_inception import CSRNet
 import torch
 
-model = InceptionResNetV2().cpu()
-checkpoint = torch.load("C:\\Users\\Admin\\Desktop\\TA\\Dataset\\0model_best.pth.tar")
+# model = InceptionResNetV2().cpu()
+model = CSRNet().cpu()
+# checkpoint = torch.load("C:\\Users\\Admin\\Desktop\\TA\\Dataset\\0model_best.pth.tar")
+checkpoint = torch.load("C:\\Users\\Admin\\Desktop\\TA\\Model\\model_best.pth.tar")
 model.load_state_dict(checkpoint['state_dict'])
 transform=transforms.Compose([
                       transforms.ToTensor(),transforms.Normalize(
