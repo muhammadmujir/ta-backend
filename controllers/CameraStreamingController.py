194ae7cf5e6637e3d1c5e5ad2c6b26a830b2b9c2
unknown <muhammadmujir011@gmail.com>
2022-07-01 15:46:23 +0700

    bugfix json write

--- controllers/CameraStreamingController.py
+++ controllers/CameraStreamingController.py
@@ -36,7 +36,7 @@ class Worker(object):
     def doWork(self):
         with app.test_request_context('/'):
             # camera = cv2.VideoCapture(data['rtspAddress'])
-            camera = cv2.VideoCapture("C:\\Users\\Admin\\Downloads\\videoplayback (1).mp4")
+            camera = cv2.VideoCapture("F:\\Backup\\Downloads\\1.mp4")
             while self.isContinue:
                 socketio.sleep(5)
                 success, frame = camera.read()
