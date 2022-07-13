85414119905436bf44199875e022558da67c899b
unknown <muhammadmujir011@gmail.com>
2022-07-13 10:50:26 +0700

    change url to postgres

--- config.py
+++ config.py
@@ -12,7 +12,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/ta_crowd_counting'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/ta_crowd_counting'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/ta_crowd_counting'
 # Turn off the Flask-SQLAlchemy event system and warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 # disable json keys sorting
